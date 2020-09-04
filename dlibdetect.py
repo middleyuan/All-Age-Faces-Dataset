6213ce198acd98d377220efd22c18f0dac0d4e37
middleyuan <alex730310@gmail.com>
2020-09-04 13:46:27 +0800

    Add ratio calculation in dlibdetect.py

--- dlibdetect.py
+++ dlibdetect.py
@@ -3,7 +3,6 @@
 import dlib
 import cv2
 FACE_PAD = 50
-DRAW_RECTANGEL = False
 
 class FaceDetectorDlib(object):
     def __init__(self, model_name, basename='frontal-face', tgtdir='.'):
@@ -11,6 +10,8 @@ class FaceDetectorDlib(object):
         self.basename = basename
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(model_name)
+        self.imgs = []
+        self.ratios = []
 
     def run(self, image_file):
         print(image_file)
@@ -29,9 +30,8 @@ class FaceDetectorDlib(object):
 
         print('%d faces detected' % len(images))
 
-        if DRAW_RECTANGEL:
-            for (x, y, w, h) in bb:
-                self.draw_rect(img, x, y, w, h)
+        for (x, y, w, h) in bb:
+            self.draw_rect(img, x, y, w, h)
                 # Fix in case nothing found in the image
         outfile = '%s/%s.jpg' % (self.tgtdir, self.basename)
         cv2.imwrite(outfile, img)
@@ -41,6 +41,9 @@ class FaceDetectorDlib(object):
         upper_cut = [min(img.shape[0], y + h + FACE_PAD), min(img.shape[1], x + w + FACE_PAD)]
         lower_cut = [max(y - FACE_PAD, 0), max(x - FACE_PAD, 0)]
         roi_color = img[lower_cut[0]:upper_cut[0], lower_cut[1]:upper_cut[1]]
+        self.imgs.append(roi_color)
+        ratio = (roi_color.shape[0] * roi_color.shape[1]) / (img.shape[0] * img.shape[1])
+        self.ratios.append(ratio)
         cv2.imwrite(name, roi_color)
         return name
 
